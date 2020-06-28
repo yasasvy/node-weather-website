45d86f7d84fbcac84c3416f5f2354e739fc443fa
yasasvy gunturu <guntur.yasasvy@gmail.com>
2020-06-28 21:44:26 +0530

    init commit

---
+++ src/app.js
@@ -0,0 +1,100 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express();
+
+//define paths for express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+//setup handlebars engine and view location
+app.set("views", viewsPath);
+app.set("view engine", "hbs");
+hbs.registerPartials(partialsPath);
+
+//setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Yasasvy",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Yasasvy",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    message: "This is testing message",
+    title: "Help",
+    name: "Yasasvy",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide the address",
+    });
+  }
+
+  geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({error})
+    }
+
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({error})
+      }
+
+     res.send({
+       forecast: forecastData,
+       location,
+       address: req.query.address
+     })
+    });
+  });
+
+})
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term",
+    });
+  }
+  res.send({
+    products: [],
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Yasasvy",
+    errorMessage: "Help article not found",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Yasasvy",
+    errorMessage: "Page not found",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("server up and running on 3000");
+});
